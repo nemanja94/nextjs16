9c0edf71a9aed633b785f497dca9f51d7730ce6f
nemanja94 <aandric.nemanja@gmail.com>
2026-09-21 10:04:58 +0200

    Home page

--- app/(shared-layout)/layout.tsx
+++ app/(shared-layout)/layout.tsx
@@ -1,10 +1,11 @@
 import { Navbar } from "@/components/web/navbar";
+import { ReactNode } from "react";
 
-export default function SharedLayout({children} : {children: React.ReactNode}) {
+export default function SharedLayout({ children }: { children: ReactNode }) {
 	return (
 		<>
-		<Navbar />
-		{children}
+			<Navbar />
+			{children}
 		</>
 	);
 }
