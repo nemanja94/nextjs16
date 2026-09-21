8f7df42b1256cb525e16d730c3b404b36b7e8fea
nemanja94 <aandric.nemanja@gmail.com>
2026-09-21 10:05:27 +0200

    Signup form

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,47 +1,124 @@
 "use client";
 
 import { signUpSchema } from "@/app/schemas/auth";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Field,
+	FieldError,
+	FieldGroup,
+	FieldLabel,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+// import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { Controller, useForm } from "react-hook-form";
+// import { toast } from "sonner";
+import z from "zod";
 
 export default function SignUpPage() {
-	const form = useForm({
+	const [isPending, startTransition] = useTransition();
+	const router = useRouter();
+	const form = useForm<z.infer<typeof signUpSchema>>({
 		resolver: zodResolver(signUpSchema),
 		defaultValues: {
-			name: "",
 			email: "",
+			name: "",
 			password: "",
-		}
+		},
 	});
+
+	function onSubmit(data: z.infer<typeof signUpSchema>) {
+		// Do something with the form values.
+		console.log(data)
+	}
 
 	return (
 		<Card>
 			<CardHeader>
-				<CardTitle>SignUp</CardTitle>
-				<CardDescription>Create Account to get started</CardDescription>
+				<CardTitle>Sign up</CardTitle>
+				<CardDescription>Create an account to get started</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<form>
-					<FieldGroup>
-						<Controller 
-							name="name" 
-							control={form.control} 
+				<form id="sign-up-form" onSubmit={form.handleSubmit(onSubmit)}>
+					<FieldGroup className="gap-y-4">
+						<Controller
+							name="name"
+							control={form.control}
 							render={({ field, fieldState }) => (
 								<Field>
 									<FieldLabel>Full Name</FieldLabel>
-									<Input placeholder="John Doe" {...field} />
+									<Input
+										aria-invalid={fieldState.invalid}
+										placeholder="John Doe"
+										{...field}
+									/>
 									{fieldState.invalid && (
 										<FieldError errors={[fieldState.error]} />
 									)}
 								</Field>
-							)}>
-						</Controller>
+							)}
+						/>
+						<Controller
+							name="email"
+							control={form.control}
+							render={({ field, fieldState }) => (
+								<Field>
+									<FieldLabel>Email</FieldLabel>
+									<Input
+										aria-invalid={fieldState.invalid}
+										placeholder="john@doe.com"
+										type="email"
+										{...field}
+									/>
+									{fieldState.invalid && (
+										<FieldError errors={[fieldState.error]} />
+									)}
+								</Field>
+							)}
+						/>
+
+						<Controller
+							name="password"
+							control={form.control}
+							render={({ field, fieldState }) => (
+								<Field>
+									<FieldLabel>Password</FieldLabel>
+									<Input
+										aria-invalid={fieldState.invalid}
+										placeholder="*****"
+										type="password"
+										{...field}
+									/>
+									{fieldState.invalid && (
+										<FieldError errors={[fieldState.error]} />
+									)}
+								</Field>
+							)}
+						/>
+
+						<Button type="submit" form="sign-up-form" disabled={isPending}>
+							{isPending ? (
+								<>
+									<Loader2 className="size-4 animate-spin" />
+									<span>Loading...</span>
+								</>
+							) : (
+								<span>Signup</span>
+							)}
+						</Button>
 					</FieldGroup>
 				</form>
 			</CardContent>
 		</Card>
-	)
+	);
 }
