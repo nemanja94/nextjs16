a7f6c3024b7fc041ad4ba527cf964d2e984939fb
nemanja94 <aandric.nemanja@gmail.com>
2026-09-25 19:04:27 +0200

    Added comments section with realtime comments

--- app/(shared-layout)/blog/[postId]/page.tsx
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -1,11 +1,12 @@
 import { buttonVariants } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { fetchQuery } from "convex/nextjs";
+import { fetchQuery, preloadQuery } from "convex/nextjs";
 import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
+import { CommentSection } from "@/components/web/CommentsSection";
 
 interface PostIdRouteProps {
   params: Promise<{
@@ -16,7 +17,12 @@ interface PostIdRouteProps {
 export default async function PostIdRoute({ params }: PostIdRouteProps) {
   const { postId } = await params;
 
-  const post = await fetchQuery(api.posts.getPostById, { postId: postId });
+  const [post, preloadedComments] = await Promise.all([
+    await fetchQuery(api.posts.getPostById, { postId: postId }),
+    preloadQuery(api.comments.getCommentsByPostId, {
+      postId: postId,
+    }),
+  ]);
 
   if (!post)
     return (
@@ -70,7 +76,7 @@ export default async function PostIdRoute({ params }: PostIdRouteProps) {
 
       <Separator className="my-8" />
 
-      {/*<CommentSection preloadedComments={preloadedComments} />*/}
+      <CommentSection preloadedComments={preloadedComments} />
     </div>
   );
 }
